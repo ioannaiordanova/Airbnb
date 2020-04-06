dd077fc0abaec83b84d40974f2f97cea3f5d8fb1
Ioanna <joanna_k4@yahoo.com>
2020-04-06 16:20:24 +0300

    SetBookingOptionsByList

--- src/test/java/com/airbnb/serenity/steps/definitions/BookingStepsDefinitions.java
+++ src/test/java/com/airbnb/serenity/steps/definitions/BookingStepsDefinitions.java
@@ -2,19 +2,15 @@ package com.airbnb.serenity.steps.definitions;
 
 import com.airbnb.serenity.entities.BookingOptions;
 import com.airbnb.serenity.steps.libraries.BookingActions;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import net.thucydides.core.annotations.Steps;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.format.TextStyle;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
+
+import static com.airbnb.serenity.utils.SetBookingOptionsByList.prepareBookingOptionsObject;
 
 
 public class BookingStepsDefinitions {
@@ -57,39 +53,7 @@ public class BookingStepsDefinitions {
 
     @And("^John has a requirements for his room:$")
     public void johnHasARequirementsForHisRoom(List<Map<String,String>> requirements) {
-     //   prepareBookingOptionsObject(bookingOptions,requirements);
-        for(Map<String, String> requirementtMap: requirements){
-            for(Map.Entry<String, String> requirement: requirementtMap.entrySet()){
-                System.out.println(requirement.getKey()+"  "+requirement.getValue());
-                if (requirement.getKey().contains("min value")){
-                    BigDecimal dc = new BigDecimal(requirement.getValue());
-                    bookingOptions.setMinPrice(dc);
-                }
-
-                if (requirement.getKey().contains("max value")){
-                    BigDecimal dc = new BigDecimal(requirement.getValue());
-                    bookingOptions.setMaxPrice(dc);
-                }
-
-                if (requirement.getKey().contains("bathrooms")){
-                    bookingOptions.setBathRooms(Integer.parseInt(requirement.getValue()));
-                }
-
-                if (requirement.getKey().contains("additional")){
-                    if (requirement.getKey().contains("Air conditioner")){
-                        bookingOptions.setAirConditioner(true);
-                    }
-                    else bookingOptions.setAirConditioner(false);
-
-                    if (requirement.getKey().contains("Jacuzzi")){
-                        bookingOptions.setJacuzzi(true);
-                    }
-                    else bookingOptions.setJacuzzi(false);
-
-                }
-
-            }
-        }
+     bookingOptions= prepareBookingOptionsObject(bookingOptions,requirements);
 
 
     }
